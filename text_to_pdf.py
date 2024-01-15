d18e9ce8d1800ae95d6616b0418aa0119e6d42eb
gokulakannan <gokulakannan.r@idp.com>
2024-01-15 17:31:34 +0530

    added multi_cell in pdf

--- text_to_pdf.py
+++ text_to_pdf.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import glob
 from fpdf import fpdf
 from pathlib import Path
@@ -9,9 +8,13 @@ pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
 for filepath in filepaths:
     pdf.add_page()
     filename = Path(filepath).stem
-    print(filename)
+    with open(filepath, "r") as file:
+        content = file.readline()
+        print(content)
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=0, h=16, txt=filename, ln=1, border=0, align="L")
+    pdf.set_font(family="Times", size=10)
+    pdf.multi_cell(w=0, h=6, txt=content, border=0, align="L")
     pdf.line(10, 21, 200, 21)
 
 pdf.output(f"PDFs/animals.pdf")
